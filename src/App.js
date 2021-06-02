f62e1b63309c35de49ce1616b29bf5aa4bd9d389
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-06-02 19:24:29 +0300

    Add routing and necessaries libraries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import './App.scss';
 
 function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+    return (
+        <Router>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/test">Not found</Link>
+            <Link to="/hell">Also not found</Link>
+            <Switch>
+                <Route exact path="/">
+                    <h1 className="bg-primary p-2 m-2 text-white text-center">Home</h1>
+                </Route>
+                <Route exact path="/about">
+                    <h1 className="bg-secondary p-2 m-2 text-white text-center">About</h1>
+                </Route>
+                <Route>
+                    <h1 className="bg-warning p-2 m-2 text-white text-center">Not found</h1>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
