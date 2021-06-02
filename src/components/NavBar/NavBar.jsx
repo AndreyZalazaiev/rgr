1b926c12b7213fd13461dba1fd423502950ff38d
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-06-02 22:46:17 +0300

    Update navbar depends on user role

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
 import { Container, Nav, Navbar as Navigationbar } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "../AuthProvider/authHooks";
+import { UserWidget } from "../UserWidget/UserWidget";
 import './NavBar.scss';
 
 function NavBar() {
+    const auth = useAuth();
     return (
         <Navigationbar bg="dark" variant="dark">
             <Container>
@@ -10,9 +13,23 @@ function NavBar() {
                 <Navigationbar.Toggle aria-controls="navbar-collapse" />
                 <Navigationbar.Collapse id="navbar-collapse">
                     <Nav className="mr-auto">
-                        <NavLink to="/" className="nav-link active">Home</NavLink>
-                        <NavLink to="/about" className="nav-link active">About</NavLink>
+                        <NavLink to="/" className="nav-link active">Главная</NavLink>
+                        { auth.user && <NavLink to="/tests" className="nav-link active">Тесты</NavLink>}
+                        { auth.user?.role === 'student' && 
+                        <>
+                        <NavLink to="/completed-tests" className="nav-link active">Пройденные тесты</NavLink>
+                        <NavLink to="/assigned-tests" className="nav-link active">Назначенные тесты</NavLink>
+                        </>
+                        }
+                        { auth.user?.role === 'teacher' && 
+                        <>
+                        <NavLink to="/completed-tests" className="nav-link active">Назначить тесты</NavLink>
+                        <NavLink to="/assigned-tests" className="nav-link active">Создать тесты</NavLink>
+                        </>
+                        }
+                        <NavLink to="/about" className="nav-link active">О проекте</NavLink>
                     </Nav>
+                    <UserWidget />
                 </Navigationbar.Collapse>
             </Container>
         </Navigationbar>
---
+++ src/components/UserWidget/UserWidget.jsx
@@ -0,0 +1,19 @@
+import { Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth } from "../AuthProvider/authHooks";
+
+function UserWidget() {
+    const auth = useAuth();
+    return (
+        auth.user &&
+        <Nav>
+            <NavDropdown title={auth.user?.username}>
+                <li><Link to='/profile' className="dropdown-item">Профиль</Link></li>
+                <NavDropdown.Divider />
+                <li><Link to='/logout' className="dropdown-item">Выйти</Link></li>
+            </NavDropdown>
+        </Nav>
+    );
+}
+
+export { UserWidget };
