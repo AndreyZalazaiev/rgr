aeb744ba640607b778facfcc32deeb40b0d6773f
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-06-05 19:10:02 +0300

    Add assigned test page

--- src/pages/AssignedTests/AssignedTests.jsx
+++ src/pages/AssignedTests/AssignedTests.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../../components/general/authHooks";
 
 function AssignedTests() {
     const { user } = useAuth();
+    const [tests, setTests] = useState([]);
+
+    // get data from the backend
+    useEffect(() => {
+        
+    }, [user]);
+
     return <h1>{user.username}</h1>;
 }
 
